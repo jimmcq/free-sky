adfc9b519da845590a46aaca4d19c41359c29b4d
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-01 22:09:11 -0800

    Initial commit
    Generated by create-react-native-app 3.8.0.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// @generated: @expo/next-adapter@2.1.52
+// Learn more: https://docs.expo.io/guides/using-nextjs/
 
-module.exports = nextConfig
+const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")(["react-native-web"]);
+
+module.exports = withPlugins([withTM, withExpo], {});
