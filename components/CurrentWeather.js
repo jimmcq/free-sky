b5b1ba1f25029aa19f659d467d54597d0e6609d1
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-06 16:07:39 -0800

    Current Weather component

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,23 +1,72 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
-function CurrentWeather({ currently, hourly, daily }) {
-  console.log(currently)
-  const { temperature, summary: currentSummary, apparentTemperature } = currently
+function CurrentWeather({ placeName, currently, hourly, daily }) {
+  const { temperature, summary: currentSummary, apparentTemperature, icon } = currently
   const { summary: hourlySummary } = hourly
   const today = daily.data[0]
   const { temperatureLow, temperatureHigh } = today
   return (
-    <View>
-      <Text>
-        {Math.round(temperature)}˚ {currentSummary}.
-      </Text>
-      <Text>
-        Feels like: {Math.round(apparentTemperature)}˚ Low: {Math.round(temperatureLow)}˚ High: {Math.round(temperatureHigh)}˚
-      </Text>
-      <Text>{hourlySummary}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Current weather for {placeName.split(',')[0]}</Text>
+      <View style={styles.row_container}>
+        <Image style={styles.icon} source={{ uri: `https://darksky.net/images/weather-icons/${icon}.png` }} alt={icon}></Image>
+
+        <View style={styles.ontainer}>
+          <Text style={styles.summary}>
+            {Math.round(temperature)}˚ {currentSummary}.
+          </Text>
+
+          <Text style={styles.text}>
+            <Text style={styles.label}>Feels like: </Text>
+            <Text style={styles.temperature}>{Math.round(apparentTemperature)}˚</Text> <Text style={styles.label}>Low: </Text>
+            <Text style={styles.temperature}>{Math.round(temperatureLow)}˚</Text> <Text style={styles.label}>High: </Text>
+            <Text style={styles.temperature}>{Math.round(temperatureHigh)}˚</Text>
+          </Text>
+        </View>
+      </View>
+      <Text style={styles.hourly_summary}>{hourlySummary}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  row_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 16,
+  },
+  summary: {
+    fontSize: 32,
+    fontWeight: 600,
+  },
+  icon: {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis: '84px',
+    width: 84,
+    height: 84,
+  },
+  hourly_summary: {
+    fontSize: 28,
+    fontWeight: 300,
+  },
+  label: {
+    fontWeight: 400,
+  },
+  temperature: {
+    fontWeight: 300,
+  },
+})
 
 export default CurrentWeather
--- pages/forecast/[coords].js
+++ pages/forecast/[coords].js
@@ -2,22 +2,24 @@ import React from 'react'
 import { ScrollView } from 'react-native'
 import CurrentWeather from '../../components/CurrentWeather'
 import { getForecast } from '../../lib/darksky'
+import { getPlaceName } from '../../lib/mapbox'
 
 export async function getServerSideProps({ query }) {
   const parts = query.coords.split(',')
   const latitude = parts[0]
   const longitude = parts[1]
   const forecast = await getForecast({ latitude, longitude })
-  const props = forecast
+  const placeName = await getPlaceName({ latitude, longitude })
+  const props = { forecast, placeName }
 
   return { props }
 }
 
-export default function App(forecast) {
+export default function App({ forecast, placeName }) {
   const { currently, hourly, daily } = forecast
   return (
     <ScrollView>
-      <CurrentWeather currently={currently} hourly={hourly} daily={daily} />
+      <CurrentWeather placeName={placeName} currently={currently} hourly={hourly} daily={daily} />
     </ScrollView>
   )
 }
