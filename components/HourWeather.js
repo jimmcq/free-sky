a97fe01a45103ce15fdbc31407acd85d08448fee
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-07 11:21:10 -0800

    Update HourWeather rain chart

--- components/HourWeather.js
+++ components/HourWeather.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement } from 'chart.js'
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 
-ChartJS.register(CategoryScale, LinearScale, BarElement)
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip)
 
 export const options = {
   responsive: true,
   maintainAspectRatio: false,
-  plugins: { legend: { display: false } },
+  plugins: {
+    legend: { display: false },
+  },
 }
 
 function HourWeather({ minutely }) {
@@ -23,8 +25,12 @@ function HourWeather({ minutely }) {
     return minuteData.precipIntensity
   })
 
+  const backgroundColor = minutely.data.map(minuteData => {
+    return minuteData.precipIntensity >= 0.02 ? '#4a80c7' : '#80a5d6'
+  })
+
   const data = { labels }
-  data.datasets = [{ barPercentage: 1.25, data: dataset, backgroundColor: 'rgba(53, 162, 235, 0.5)' }]
+  data.datasets = [{ barPercentage: 1.25, data: dataset, backgroundColor }]
 
   return (
     <View style={styles.container}>
