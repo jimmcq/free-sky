c5fd04ba22383c2769e0288cd37b4abc79629a7b
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-08 00:20:04 -0800

    Location search

---
+++ pages/api/searchplace.js
@@ -0,0 +1,19 @@
+import { searchPlace } from '../../lib/mapbox'
+
+async function handler(req, res) {
+  const { place } = req.query
+
+  if (req.method === 'GET' && place) {
+    const features = await searchPlace(place)
+    if (features.length) {
+      const result = features.map(place => {
+        return { place_name: place.place_name, center: place.center }
+      })
+      res.status(200).send(result)
+    }
+  } else {
+    res.status(404).send([])
+  }
+}
+
+export default handler
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
 // @generated: @expo/next-adapter@2.1.52
-import React from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-web'
+import { searchPlace } from '../lib/mapbox'
 
-export default function App() {
+function IndexPage() {
+  const [linkList, setLinkList] = useState()
+
+  async function handleSubmit(event) {
+    const text = event.nativeEvent.text
+    if (!text) {
+      return null
+    }
+    const url = `/api/searchplace?place=${text}`
+    const response = await fetch(url)
+    const features = await response.json()
+
+    if (features.length) {
+      setLinkList(
+        features.map((place, index) => {
+          const latitude = place.center[1]
+          const longitude = place.center[0]
+          return (
+            <Link key={index} href={`/forecast/${latitude},${longitude}`}>
+              <Text>{place.place_name}</Text>
+            </Link>
+          )
+        })
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>When Dark Sky is gone, Free-Sky.Net remains</Text>
+      <TextInput style={styles.input} placeholder="Location Search" onSubmitEditing={handleSubmit}></TextInput>
+      <View>{linkList}</View>
     </View>
   )
 }
@@ -19,4 +50,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 })
+
+export default IndexPage
