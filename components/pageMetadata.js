3d462403dfdcc497c471d8a39fdfb337dcc6d015
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-20 16:02:14 -0800

    Updates for @expo/next-adapter

--- components/pageMetadata.js
+++ components/pageMetadata.js
@@ -10,7 +10,6 @@ function PageMetadata({
       <title key="title">{title}</title>
       <meta key="description" name="description" content={description} />
       <meta key="robots" name="robots" content={robots} />
-      <meta name="viewport" content="width=375, initial-scale=1"></meta>
     </Head>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import PageMetadata from '../components/pageMetadata'
 
 function FreeSky({ Component, pageProps }) {
   const { pageMetadata } = pageProps
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <PageMetadata {...pageMetadata} />
       <Component {...pageProps} />
     </>
