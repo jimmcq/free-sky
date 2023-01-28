fa487071f4aea5cd8944a99d3d07a7e1c8704fff
Jim McQuillan <james_mcquillan@ziffdavis.com>
2023-01-27 21:49:58 -0800

    fix:normalizeCoordinates

--- tests/lib/helpers.test.js
+++ tests/lib/helpers.test.js
@@ -1,4 +1,4 @@
-import { bearingToCardinal } from '../../lib/helpers'
+import { bearingToCardinal, normalizeCoordinates } from '../../lib/helpers'
 
 describe('bearingToCardinal', () => {
   it('should return "N" for 0 and 360 degrees', () => {
@@ -33,5 +33,26 @@ describe('bearingToCardinal', () => {
   it('should return "NW" for 315 degrees', () => {
     expect.assertions(1)
     expect(bearingToCardinal(315)).toBe('NW')
+  })
+})
+
+describe('normalizeCoordinates', () => {
+  it('should return the same coordinates extended to 4 decimals for valid coordinates', () => {
+    expect.assertions(1)
+    expect(normalizeCoordinates({ latitude: 0, longitude: 0 })).toEqual({
+      latitude: '0.0000',
+      longitude: '0.0000',
+    })
+  })
+  it('should return the same coordinates shortened to 4 decimals for valid coordinates', () => {
+    expect.assertions(1)
+    expect(normalizeCoordinates({ latitude: 0.123456789, longitude: 0.123456789 })).toEqual({
+      latitude: '0.1235',
+      longitude: '0.1235',
+    })
+  })
+  it('should throw an error for invalid coordinates', () => {
+    expect.assertions(1)
+    expect(() => normalizeCoordinates({ latitude: 0, longitude: 200 })).toThrow('Invalid location coordinates')
   })
 })
