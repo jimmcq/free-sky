7ae708fbec049cb7118ca6d14d81aebb2b603db0
Jim McQuillan <jim@jimmcq.com>
2023-02-04 10:01:07 -0800

    Mapbox to TypeScript

--- src/lib/mapbox.ts
+++ src/lib/mapbox.ts
@@ -1,7 +1,7 @@
 import { cacheGet, cacheSet, safeKey } from './cache'
 import { normalizeCoordinates } from './helpers'
 
-async function getPlaceName({ latitude, longitude }) {
+async function getPlaceName({ latitude, longitude }: { latitude: string; longitude: string }) {
   const { latitude: sanitizedLatitude, longitude: sanitizedLongitude } = normalizeCoordinates({ latitude, longitude })
 
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${sanitizedLongitude},${sanitizedLatitude}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
@@ -27,7 +27,7 @@ async function getPlaceName({ latitude, longitude }) {
   return result
 }
 
-async function searchPlace(place) {
+async function searchPlace(place: string) {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
   const cacheKey = safeKey(url)
   let result = {}
